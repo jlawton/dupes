d5d43dca894d988b1b72174a424b7334238ad86d
James Lawton <james.lawton@gmail.com>
2016-04-02 16:37:42 -0700

    list --bare -0

--- dupes/ListCommand.swift
+++ dupes/ListCommand.swift
@@ -9,12 +9,12 @@
 import Foundation
 
 extension DupesDatabase {
-    func listDuplicates(indent: String = "") throws -> AnySequence<String> {
+    func listDuplicates(indent: String = "", bare: Bool = false, separator: String = "\n") throws -> AnySequence<String> {
         let groupedSequence = try groupedDuplicates()
         return AnySequence {
             return groupedSequence.lazy.map({ files in
-                let s = "\(indent)[Files of size \(human(files[0].size))]\n"
-                return s + files.map({ "\(indent)\($0.path)" }).joinWithSeparator("\n")
+                let s = bare ? "" : "\(indent)[Files of size \(human(files[0].size))]\(separator)"
+                return s + files.map({ "\(indent)\($0.path)" }).joinWithSeparator(separator)
             }).generate()
         }
     }
@@ -27,26 +27,38 @@ struct ListCommand: CommandType {
     let function = "List all duplicates"
 
     func run(options: ListCommandOptions) -> Result<(), DupesError> {
-        return DupesDatabase.open(options.dbPath).tryMap { db in
-            var first = true
-            for group in try db.listDuplicates() {
-                if first { first = false }
-                else { print("") }
-                print(group)
-            }
+        return DupesDatabase.open(options.dbPath).tryMap({ try ListCommand.run($0, bare: options.bare, fileSeparator: options.fileSeparator) })
+    }
+
+    static func run(db: DupesDatabase, bare: Bool = false, fileSeparator: String = "\n", groupSeparator optionalGroupSeparator: String? = nil) throws {
+        var first = true
+        let groupSeparator = optionalGroupSeparator ?? "\(fileSeparator)\(fileSeparator)"
+        for group in try db.listDuplicates("", bare: bare, separator: fileSeparator) {
+            if first { first = false }
+            else { print(groupSeparator, separator: "", terminator: "") }
+            print(group, separator: "", terminator: "")
         }
     }
 }
 
 struct ListCommandOptions: OptionsType {
     let dbPath: String
+    let bare: Bool
+    let fileSeparator: String
 
-    static func create(dbPath: String) -> ListCommandOptions {
-        return ListCommandOptions(dbPath: dbPath)
+    static func create(dbPath: String) -> Bool -> Bool -> ListCommandOptions {
+        return { bare in { nulSeparator in
+            let fileSeparator = nulSeparator ? "\0" : "\n"
+            return ListCommandOptions(dbPath: dbPath,
+                bare: (bare || nulSeparator),
+                fileSeparator: fileSeparator)
+        } }
     }
 
     static func evaluate(m: CommandMode) -> Result<ListCommandOptions, CommandantError<DupesError>> {
         return create
             <*> m <| databaseOption
+            <*> m <| Switch(flag: nil, key: "bare", usage: "Display grouped file paths without summary data")
+            <*> m <| Switch(flag: "0", key: "print0", usage: "Separate files with NUL characters, rather than by line. Implies --bare")
     }
 }
