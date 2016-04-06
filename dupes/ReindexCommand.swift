7afc79d4010a4551e57713e06d9b4fee8726a0d1
James Lawton <james.lawton@gmail.com>
2016-04-05 18:45:12 -0700

    reindex --hash

--- dupes/ReindexCommand.swift
+++ dupes/ReindexCommand.swift
@@ -13,28 +13,37 @@ struct ReindexCommand: CommandType {
     let function = "Unindex all deleted duplicates and unhash changed duplicates"
 
     func run(options: ReindexCommandOptions) -> Result<(), DupesError> {
-        return DupesDatabase.open(options.db.path).tryMap { db in
-            try db.reIndex(duplicatesOnly: !options.allFiles)
-
-            // Since we're housekeeping, try to free up space and reduce fragmentation
-            do { try db.vacuum(force: true) } catch {}
-        }
+        return DupesDatabase.open(options.db.path)
+            .tryPassthrough { db in
+                try db.reIndex(duplicatesOnly: !options.allFiles)
+            }
+            .tryPassthrough { db in
+                if options.hash {
+                    try HashCommand.run(db)
+                }
+            }
+            .tryMap { db in
+                // Since we're housekeeping, try to free up space and reduce fragmentation
+                do { try db.vacuum(force: true) } catch {}
+            }
     }
 }
 
 struct ReindexCommandOptions: OptionsType {
     let db: DatabaseOptions
+    let hash: Bool
     let allFiles: Bool
 
-    static func create(db: DatabaseOptions) -> Bool -> ReindexCommandOptions {
-        return { allFiles in
-            ReindexCommandOptions(db: db, allFiles: allFiles)
-        }
+    static func create(db: DatabaseOptions) -> Bool -> Bool -> ReindexCommandOptions {
+        return { hash in { allFiles in
+            ReindexCommandOptions(db: db, hash: hash, allFiles: allFiles)
+        } }
     }
 
     static func evaluate(m: CommandMode) -> Result<ReindexCommandOptions, CommandantError<DupesError>> {
         return create
             <*> DatabaseOptions.evaluate(m)
+            <*> m <| Option(key: "hash", defaultValue: false, usage: "Hash the potential duplicates after reindexing")
             <*> m <| Switch(flag: "a", key: "all", usage: "Scan all indexed files, not just duplicates")
     }
 }
